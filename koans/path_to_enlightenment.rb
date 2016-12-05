d07157e6fff681946cdd6769220f5281f9564114
Pedro Lafuente <pedro.lafuente@gmail.com>
2016-12-05 16:45:16 +0100

    First steps

--- koans/path_to_enlightenment.rb
+++ koans/path_to_enlightenment.rb
@@ -2,13 +2,13 @@
 
 $LOAD_PATH << File.dirname(__FILE__)
 
-require 'about_asserts'
-require 'about_nil'
-require 'about_objects'
-require 'about_arrays'
-require 'about_array_assignment'
-require 'about_hashes'
-require 'about_strings'
+require 'about_asserts' #Done
+require 'about_nil'  #Done
+require 'about_objects' #Done
+require 'about_arrays' #Done
+require 'about_array_assignment' #Done
+require 'about_hashes' #Done
+require 'about_strings' #Done
 require 'about_symbols'
 require 'about_regular_expressions'
 require 'about_methods'
